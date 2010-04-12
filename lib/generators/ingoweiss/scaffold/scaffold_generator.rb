1d65be7d6710173789185238389bd509414d062a
Ingo Weiss <ingo@ingoweiss.com>
2010-04-12 11:43:43 +0200

    Factored out ingoweiss:scaffold_controller generator

--- lib/generators/ingoweiss/scaffold/scaffold_generator.rb
+++ lib/generators/ingoweiss/scaffold/scaffold_generator.rb
@@ -1,24 +1,24 @@
 require 'rails/generators/resource_helpers'
 require 'rails/generators/migration'
+require File.join(File.dirname(__FILE__), '../helpers/scope_helper') 
 
 module Ingoweiss
   class ScaffoldGenerator < Rails::Generators::NamedBase
     include Rails::Generators::ResourceHelpers
     include Rails::Generators::Migration
+    include ScopeHelper
   
     argument :attributes, :type => :array, :required => false, :banner => 'field:type field:type'
     class_option :scope, :type => :array, :default => [], :banner => 'grand_parent parent', :desc => 'Indicate parent resource(s) if nested'
     class_option :singleton, :type => :boolean, :default => false, :desc => 'Is this a singleton resource?'
     class_option :skip_route, :type => :boolean, :default => false, :desc => 'Do not add route to config/routes.rb'
+    
+    hook_for :scaffold_controller
   
     def self.source_root
       @source_root ||= File.expand_path('../../templates', __FILE__)
     end
-  
-    def generate_controllers
-      template 'controller.erb', "app/controllers/#{scoped_controller_plural_name}_controller.rb"
-    end
-    
+      
     def add_routes
       return if options[:skip_route]
       route "resource#{'s' unless options[:singleton]} :#{options[:singleton] ? singular_name : plural_name}"
@@ -60,27 +60,6 @@ module Ingoweiss
     
     private
     
-    def scope
-      options[:scope]
-    end
-    
-    def scoped?
-      scope.any?
-    end
-    
-    def unscoped?
-      !scoped?
-    end
-    
-    def singleton?
-      options[:singleton]
-    end
-    
-    # Example: 'post_comment_' for post_comment_approval_path
-    def scope_prefix
-      scope.collect{|s| s.singularize + '_'}.join
-    end
-    
     # Example: '@post, @comment, approval'
     def instance_variable_scope(variable=nil)
       instance_variables = scope.collect{|s| '@' + s.singularize}
@@ -96,84 +75,6 @@ module Ingoweiss
     # Examples: 'PostComments', 'PostCommentApproval'
     def scoped_controller_class_name
       scoped_controller_plural_name.camelize
-    end
-    
-    def controller_retrieve_scope
-      lines = []
-      scope.each_with_index do |scope_item, index|
-        if index == 0
-          lines << "@#{scope_item.singularize} = #{scope_item.singularize.classify}.find(params[:#{scope_item.singularize}_id])"
-        else
-          previous_scope_item = scope[index-1]
-          if scope_item.pluralize == scope_item
-            lines << "@#{scope_item.singularize} = @#{previous_scope_item.singularize}.#{scope_item.pluralize}.find(params[:#{scope_item.singularize}_id])"
-          else
-            lines << "@#{scope_item.singularize} = @#{previous_scope_item.singularize}.#{scope_item.singularize}"
-          end
-        end
-      end
-      lines
-    end
-    
-    def controller_retrieve_resource
-      if scope.any?
-        if options[:singleton]
-          "@#{name.singularize} = @#{scope.last.singularize}.#{name.singularize}"
-        else
-          "@#{name.singularize} = @#{scope.last.singularize}.#{name.pluralize}.find(params[:id])"
-        end
-      else
-        "@#{name.singularize} = #{name.singularize.classify}.find(params[:id])"
-      end
-    end
-    
-    def controller_retrieve_collection
-      if scope.any?
-        "@#{name.pluralize} = @#{scope.last.singularize}.#{name.pluralize}"
-      else
-        "@#{name.pluralize} = #{name.singularize.classify}.all"
-      end
-    end
-    
-    def controller_build_resource
-      if scope.any?
-        if options[:singleton]
-          "@#{name.singularize} = @#{scope.last.singularize}.build_#{name.singularize}(params[:#{name.singularize}])"
-        else
-          "@#{name.singularize} = @#{scope.last.singularize}.#{name.pluralize}.build(params[:#{name.singularize}])"
-        end
-      else
-        "@#{name.singularize} = #{name.singularize.classify}.new(params[:#{name.singularize}])"
-      end
-    end
-    
-    def controller_create_resource
-      if scope.any?
-        if options[:singleton]
-          "@#{name.singularize} = @#{scope.last.singularize}.create_#{name.singularize}(params[:#{name.singularize}])"
-        else
-          "@#{name.singularize} = @#{scope.last.singularize}.#{name.pluralize}.create(params[:#{name.singularize}])"
-        end
-        
-      else
-        "@#{name.singularize} = #{name.singularize.classify}.create(params[:#{name.singularize}])"
-      end
-    end
-    
-    def controller_destroy_resource
-      "@#{name.singularize}.destroy"
-    end
-    
-    def controller_update_resource
-      "@#{name.singularize}.update_attributes(params[:#{name.singularize}])"
-    end
-    
-    def controller_respond_with_collection
-      "respond_with #{(scope.collect{|scope_item| '@' + scope_item.singularize} + ['@' + name.pluralize]).join(', ')}"
-    end
-  
-    def controller_respond_with_resource
-      "respond_with #{(scope.collect{|scope_item| '@' + scope_item.singularize} + ['@' + name.singularize]).join(', ')}"
     end
   
   end
---
+++ lib/generators/ingoweiss/scaffold_controller/scaffold_controller_generator.rb
@@ -0,0 +1,110 @@
+require File.join(File.dirname(__FILE__), '../helpers/scope_helper')
+
+module Ingoweiss
+  class ScaffoldControllerGenerator < Rails::Generators::NamedBase
+    
+    include ScopeHelper
+    
+    class_option :scope, :type => :array, :default => [], :banner => 'grand_parent parent', :desc => 'Indicate parent resource(s) if nested'
+    class_option :singleton, :type => :boolean, :default => false, :desc => 'Is this a singleton resource?'
+  
+    def self.source_root
+      @source_root ||= File.expand_path('../templates', __FILE__)
+    end
+  
+    def generate_controller
+      template 'controller.erb', "app/controllers/#{scoped_controller_plural_name}_controller.rb"
+    end
+    
+    private
+    
+    # Examples: 'post_comments', 'post_comment_approval'
+    def scoped_controller_plural_name
+      scope_prefix + (options.singleton? ? singular_name : plural_name)
+    end
+    
+    # Examples: 'PostComments', 'PostCommentApproval'
+    def scoped_controller_class_name
+      scoped_controller_plural_name.camelize
+    end
+    
+    def controller_retrieve_scope
+      lines = []
+      options.scope.each_with_index do |scope_item, index|
+        if index == 0
+          lines << "@#{scope_item.singularize} = #{scope_item.singularize.classify}.find(params[:#{scope_item.singularize}_id])"
+        else
+          previous_scope_item = scope[index-1]
+          if scope_item.pluralize == scope_item
+            lines << "@#{scope_item.singularize} = @#{previous_scope_item.singularize}.#{scope_item.pluralize}.find(params[:#{scope_item.singularize}_id])"
+          else
+            lines << "@#{scope_item.singularize} = @#{previous_scope_item.singularize}.#{scope_item.singularize}"
+          end
+        end
+      end
+      lines
+    end
+    
+    def controller_retrieve_resource
+      if options.scope.any?
+        if options[:singleton]
+          "@#{name.singularize} = @#{scope.last.singularize}.#{name.singularize}"
+        else
+          "@#{name.singularize} = @#{scope.last.singularize}.#{name.pluralize}.find(params[:id])"
+        end
+      else
+        "@#{name.singularize} = #{name.singularize.classify}.find(params[:id])"
+      end
+    end
+    
+    def controller_retrieve_collection
+      if options.scope.any?
+        "@#{name.pluralize} = @#{scope.last.singularize}.#{name.pluralize}"
+      else
+        "@#{name.pluralize} = #{name.singularize.classify}.all"
+      end
+    end
+    
+    def controller_build_resource
+      if options.scope.any?
+        if options[:singleton]
+          "@#{name.singularize} = @#{scope.last.singularize}.build_#{name.singularize}(params[:#{name.singularize}])"
+        else
+          "@#{name.singularize} = @#{scope.last.singularize}.#{name.pluralize}.build(params[:#{name.singularize}])"
+        end
+      else
+        "@#{name.singularize} = #{name.singularize.classify}.new(params[:#{name.singularize}])"
+      end
+    end
+    
+    def controller_create_resource
+      if options.scope.any?
+        if options[:singleton]
+          "@#{name.singularize} = @#{scope.last.singularize}.create_#{name.singularize}(params[:#{name.singularize}])"
+        else
+          "@#{name.singularize} = @#{scope.last.singularize}.#{name.pluralize}.create(params[:#{name.singularize}])"
+        end
+        
+      else
+        "@#{name.singularize} = #{name.singularize.classify}.create(params[:#{name.singularize}])"
+      end
+    end
+    
+    def controller_destroy_resource
+      "@#{name.singularize}.destroy"
+    end
+    
+    def controller_update_resource
+      "@#{name.singularize}.update_attributes(params[:#{name.singularize}])"
+    end
+    
+    def controller_respond_with_collection
+      "respond_with #{(options.scope.collect{|scope_item| '@' + scope_item.singularize} + ['@' + name.pluralize]).join(', ')}"
+    end
+  
+    def controller_respond_with_resource
+      "respond_with #{(options.scope.collect{|scope_item| '@' + scope_item.singularize} + ['@' + name.singularize]).join(', ')}"
+    end          
+    
+  end
+end
